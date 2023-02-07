38abd27f8a47f247b741794ccd842224bc8befc7
Diego Costa Alves <61090568+diegobachsi@users.noreply.github.com>
2023-02-07 20:33:21 -0300

    Create urls.py

---
+++ tópicos/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'topicos'
+
+urlpatterns = [
+    path('', views.topicos, name='index'),
+    path('<slug:slug>', views.details, name='details'),
+    path('<str:name>', views.title, name='title'),
+]
