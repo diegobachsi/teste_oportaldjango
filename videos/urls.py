650ddafd208eea7fb926f31903f39c65ef7f9b2c
Diego Costa Alves <61090568+diegobachsi@users.noreply.github.com>
2023-02-08 17:10:20 -0300

    Update urls.py

--- videos/urls.py
+++ videos/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', views.videos, name='index'),
     path('<slug:slug>', views.details, name='details'),
     path('<int:id>/<slug:slug>', views.details, name='details'),
-    path('<int:id>/<title>', views.video_assistido, name='video_assistido')
+    path('<int:id>/(?P<title>[\w\-]+)/$', views.video_assistido, name='video_assistido')
 ]
